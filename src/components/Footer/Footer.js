bdfade21a0b98144bd34de3741486bd187c2f75e
missveev <vivianpamilerin@gmail.com>
2021-01-18 13:36:27 +0100

    updated file

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
 
         return (
             <footer className="bg-near-black white-80 pv5 pv6-l ph4">
-                <p className="f6"><span className="dib mr4 mr5-ns">©2021 LazyWeirdVeevDev.</span>
+                <p className="f6"><span className="dib mr4 mr5-ns">©{(new Date().getFullYear())} LazyWeirdVeevDev.</span>
                     <a className="link white-80 hover-light-purple" href="/terms">Terms</a> /
                     <a className="link white-80 hover-gold" href="/privacy"> Privacy </a> /
                     <a className="link white-80 hover-green" href="#">vivianpamilerin@gmail.com </a>
