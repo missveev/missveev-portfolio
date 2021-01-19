1a5d17b6e764de9bad11ba19992ab1999301d07a
missveev <vivianpamilerin@gmail.com>
2021-01-19 09:39:41 +0100

    updating file

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -14,10 +14,10 @@ class Navigation extends Component {
 
             <header id="home" className="sans-serif">
                 <div className="cover bg-left bg-center-l">
-                    <div>
-                        <nav id="nav-wrap" className="dt w-100 mw8 center" style={{display: "block;"}}>
 
-                            <div className="dtc v-mid tr pa3">
+                        <nav id="nav-wrap" className="dt w-100 mw8 center" >
+
+                            <div className="nav">
                                 <a className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3"
                                    href="#home">HOME</a>
                                 <a className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3"
@@ -31,17 +31,20 @@ class Navigation extends Component {
                             </div>
 
                         </nav>
-
-                        <div className="tc-l mt4 mt5-m mt6-l ph3">
-                            <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Hello, I'm <span style={{color: '#204051'}}>{name}</span></h1>
-                            <h2 className="fw1 f3 white-80 mt3 mb4">{description}</h2>
+                        <div className="row banner">
+                        <div className="banner-text">
+                            <h1 >Hello, I'm <span style={{color: '#204051'}}>{name}</span></h1>
+                            <h3 >{description}</h3>
+                            <div className="socials">
                             <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3"
                                href={project}>Projects</a>
                             &emsp;
                             <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3"
                                href={github}>Github</a>
                         </div>
-                    </div>
+                        </div>
+                        </div>
+
                 </div>
             </header>
         );
